5e4339246b4b9cf0d3d6960d74e68800fe5f8fd8
tic40 <ccpzjoh@gmail.com>
2022-05-05 20:32:32 +0900

    WIP

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false }
-
     return config
   },
   reactStrictMode: true,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,72 @@
 import { useEffect, useRef } from 'react'
 
-import Meta from '../components/meta'
 import type { NextPage } from 'next'
 import * as THREE from 'three'
 import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader'
+import { MMDAnimationHelper } from 'three/examples/jsm/animation/MMDAnimationHelper'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Meta from '../components/meta'
+import Ammo from 'ammojs-typed'
 
 const Home: NextPage = () => {
   const mountRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const loader = new MMDLoader()
-    const renderer = new THREE.WebGLRenderer()
+    const w = window.innerWidth
+    const h = window.innerHeight
 
-    const elm = mountRef.current
+    Ammo().then((Ammo) => {
+      Ammo = Ammo
+      const renderer = new THREE.WebGLRenderer()
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(w, h)
+      renderer.setClearColor(new THREE.Color(0xffffff))
+      mountRef.current?.appendChild(renderer.domElement)
 
-    elm?.appendChild(renderer.domElement)
-    const w = window.innerWidth // elm?.clientWidth || 500
-    const h = window.innerHeight //Math.max(500, elm?.clientHeight || 0)
+      const scene = new THREE.Scene()
+      // camera
+      const camera = new THREE.PerspectiveCamera(50, w / h, 2, 2000)
+      camera.position.set(0, 20, 45)
+      // ambientLight
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+      scene.add(ambientLight)
+      // directionalLight
+      const directionalLight = new THREE.DirectionalLight(0xffe2b9, 0.4)
+      directionalLight.position.set(1, 0.75, 0.5).normalize()
+      scene.add(directionalLight)
+      // grid
+      const gridHelper = new THREE.GridHelper(1000, 100)
+      scene.add(gridHelper)
+      scene.background = new THREE.Color(0xf0f0f0)
+      // control
+      const controls = new OrbitControls(camera, renderer.domElement)
+      const clock = new THREE.Clock()
 
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(w, h)
-
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(45, w / h, 1, 100)
-    camera.position.set(0, 0, 100)
-    const geometry = new THREE.SphereGeometry(300, 30, 30)
-
-    loader.load(
-      '/models/miku_lat/Lat式ミクVer2.31_Normal.pmd',
-      // called when the resource is loaded
-      (mesh: THREE.Object3D<THREE.Event>) => {
-        scene.add(mesh)
-      },
-      // called when loading is in progresses
-      (xhr) => {
-        console.info((xhr.loaded / xhr.total) * 100 + '% loaded')
+      const helper = new MMDAnimationHelper()
+      // load model
+      const loader = new MMDLoader()
+      loader.loadWithAnimation(
+        '/models/lat_miku/Lat式ミクVer2.31_White.pmd',
+        '/motions/schrodingeiger_no_koneko/Schrodingeiger_no_Koneko1.vmd',
+        // called when the resource is loaded
+        ({ mesh, animation }) => {
+          helper.add(mesh, {
+            animation: animation,
+            physics: true,
+          })
+          scene.add(mesh)
+        },
+        // called when loading is in progresses
+        (xhr) => console.info((xhr.loaded / xhr.total) * 100 + '% loaded'),
+        (e) => console.error(e)
+      )
+      const t = () => {
+        helper.update(clock.getDelta())
+        renderer.render(scene, camera)
+        requestAnimationFrame(t)
       }
-    )
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff)
-    directionalLight.position.set(1, 1, 1)
-
-    scene.add(directionalLight)
-
-    const tick = () => {
-      // mesh.rotation.y += 0.01
-      renderer.render(scene, camera)
-
-      requestAnimationFrame(tick)
-    }
-
-    tick()
-
-    return () => {
-      elm?.removeChild(renderer.domElement)
-    }
+      t()
+    })
   }, [])
 
   return (
