a8fc8178ebc9cbcca9a56570071d63b9467116b0
tic40 <ccpzjoh@gmail.com>
2022-05-06 14:36:10 +0900

    Add Alicia model

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,16 @@ import type { NextPage } from 'next'
 import { useEffect, useRef, useState } from 'react'
 import {
   AmbientLight,
+  AxesHelper,
+  Box3,
   Clock,
-  Color,
   DirectionalLight,
   GridHelper,
+  Mesh,
   PerspectiveCamera,
+  PlaneGeometry,
   Scene,
+  ShadowMaterial,
   WebGLRenderer,
 } from 'three'
 import { MMDAnimationHelper } from 'three/examples/jsm/animation/MMDAnimationHelper'
@@ -16,9 +20,28 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Ammo from 'ammojs-typed'
 import Meta from '../components/meta'
 
+const MODELS = [
+  {
+    path: './models/lat_miku/Lat式ミクVer2.31_White.pmd',
+    height: 1.58,
+  },
+  {
+    path: './models/Alicia/MMD/Alicia_solid.pmx',
+    height: 1.48,
+  },
+]
+const MOTIONS = [
+  {
+    path: './motions/schrodingeiger_no_koneko/Schrodingeiger_no_Koneko1.vmd',
+  },
+]
+
 const Home: NextPage = () => {
   const mountRef = useRef<HTMLDivElement>(null)
   const [loaded, setLoaded] = useState(false)
+
+  const model = MODELS[0]
+  const motion = MOTIONS[0]
 
   useEffect(() => {
     const w = window.innerWidth
@@ -29,34 +52,62 @@ const Home: NextPage = () => {
       const renderer = new WebGLRenderer()
       renderer.setPixelRatio(window.devicePixelRatio)
       renderer.setSize(w, h)
+      renderer.shadowMap.enabled = true
+      renderer.setClearColor(0xffffff, 1.0)
       mountRef.current?.appendChild(renderer.domElement)
 
       const scene = new Scene()
       const clock = new Clock()
       const helper = new MMDAnimationHelper()
+
       // camera
-      const camera = new PerspectiveCamera(50, w / h, 2, 2000)
-      camera.position.set(0, 10, 50)
+      const camera = new PerspectiveCamera(45, w / h, 0.1, 100)
+
       // ambientLight
-      const ambientLight = new AmbientLight(0xffffff, 0.6)
-      scene.add(ambientLight)
+      scene.add(new AmbientLight(0xffffff, 0.6))
+
       // directionalLight
-      const directionalLight = new DirectionalLight(0xffe2b9, 0.4)
-      directionalLight.position.set(1, 0.75, 0.5).normalize()
+      const directionalLight = new DirectionalLight(0xffe2b9, 0.5)
+      directionalLight.position.set(2, 4, 2)
+      directionalLight.castShadow = true
       scene.add(directionalLight)
+
+      // ground
+      const groundMesh = new Mesh(
+        new PlaneGeometry(10, 10, 1, 1),
+        new ShadowMaterial({ opacity: 0.2 })
+      )
+      groundMesh.rotation.x = -Math.PI / 2
+      groundMesh.receiveShadow = true
+      scene.add(groundMesh)
+
       // grid
-      const gridHelper = new GridHelper(1000, 100)
-      scene.add(gridHelper)
-      scene.background = new Color(0xf0f0f0)
+      scene.add(new GridHelper(10, 20, 0x0000000, 0x999999))
+      scene.add(new AxesHelper(10))
+
       // control
-      new OrbitControls(camera, renderer.domElement)
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.enableDamping = true
 
       // load model
       const loader = new MMDLoader()
       loader.loadWithAnimation(
-        './models/lat_miku/Lat式ミクVer2.31_White.pmd',
-        './motions/schrodingeiger_no_koneko/Schrodingeiger_no_Koneko1.vmd',
+        model.path,
+        motion.path,
         ({ mesh, animation }) => {
+          mesh.castShadow = true
+          // mesh.receiveShadow = true
+          const boundingBox = new Box3().setFromObject(mesh)
+          const m = model.height / boundingBox.max.y
+          mesh.scale.multiplyScalar(m)
+          const boundingBoxScaled = new Box3().setFromObject(mesh)
+          controls.target.set(0, model.height / 2, 0)
+          controls.object.position.set(
+            0,
+            model.height * 0.55,
+            model.height * 1.9
+          )
+
           helper.add(mesh, {
             animation: animation,
             physics: true,
@@ -69,6 +120,7 @@ const Home: NextPage = () => {
       )
       const t = () => {
         helper.update(clock.getDelta())
+        controls.update()
         renderer.render(scene, camera)
         requestAnimationFrame(t)
       }
@@ -94,7 +146,7 @@ const Home: NextPage = () => {
           rounded-full
         "
         role="status"
-        style={loaded ? { display: 'none' } : { display: 'block' }}
+        style={{ display: loaded ? 'none' : 'block' }}
       ></div>
       <a
         className="
